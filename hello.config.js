4edd30f6bc44c4dee54a090031d80f6fb0eea057
Brian <git@washersclub.org>
2024-01-23 15:40:52 -0600

    quickstart

--- hello.config.js
+++ hello.config.js
@@ -5,3 +5,6 @@ const config = {
 //    provider_hint:[]
 }
 module.exports = config
+
+// added by @hellocoop/quickstart --nextjs on 2024-01-23T21:19:09.200Z
+config.client_id = 'dec70bb6-6ffa-461b-b22e-2ff23ba5887a'
